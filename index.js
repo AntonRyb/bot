908074c18247268f115b0703610045f466e062cf
AntonRyb <33671099+AntonRyb@users.noreply.github.com>
2019-06-17 16:38:56 +0300

    Update and rename bot.js to index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 const token = '608783806:AAEIm9YHSELLgbNGz8Q6Ib4cmmwS48ly4Tk';
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+// Create a bot that uses 'polling' to fetch new updates  -- , {polling: true}
+const bot = new TelegramBot(token);
 
 
 let button = {
@@ -39,4 +39,22 @@ bot.on('callback_query', query => {
      let answer = `${result.ccy} => ${result.base_ccy}\nBuy: ${result.buy}\nSale: ${result.sale}`;
      bot.sendMessage(id, answer);
   })
-})
+});
+
+module.exports = (req, res) => {
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        if (body) {
+            bot.processUpdate(JSON.parse(body));
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(body);            
+        }
+      else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('OK');
+      }
+    });
+};
